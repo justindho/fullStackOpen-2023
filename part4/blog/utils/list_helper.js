89b5df71c4054f10590b9f17afe688b9d43c60cc
Justin Ho <justinho515@gmail.com>
2023-05-17 17:47:12 -0700

    Add exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,9 +1,28 @@
+const Blog = require('../models/blog')
+
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => {
     return sum + item.likes
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  let maxBlog = null
+  let maxLikes = 0
+  blogs.forEach(blog => {
+    if (blog.likes > maxLikes) {
+      maxBlog = new Blog(blog).toJSON()
+      maxLikes = blog.likes
+    }
+  })
+  console.log(maxBlog)
+  return maxBlog
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
